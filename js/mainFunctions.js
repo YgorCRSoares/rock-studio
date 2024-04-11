568a6051bc01a5a77b0bae0b1ca6bf9c2061674d
Ygor Soares <155658322+YgorCRSoares@users.noreply.github.com>
2024-04-11 18:52:40 -0300

    Teste mainFunctions.js

--- js/mainFunctions.js
+++ js/mainFunctions.js
@@ -1,3 +1,10 @@
+window.addEventListener("orientationchange", function() {
+    // Evita o recarregamento da página
+    event.preventDefault();
+    // Executa qualquer outra ação necessária quando a orientação da tela muda
+    // Por exemplo, redimensionar elementos na página para se adaptar à nova orientação
+}, false);
+
 // Toggle class menu
 $(function () {
   $('.menu').on('click', function () {
